033c47582d766993a7ffad8fd85dae7fa201e1b9
Dick Dijk <dick.dijk@ortec-finance.com>
2015-10-07 12:04:56 +0200

    add Fibonacci generator

--- src/main/java/nl/ortecfinance/opal/weblogicworkmanager/RequestProcessor.java
+++ src/main/java/nl/ortecfinance/opal/weblogicworkmanager/RequestProcessor.java
@@ -1,10 +1,7 @@
 package nl.ortecfinance.opal.weblogicworkmanager;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
@@ -13,50 +10,57 @@ import javax.ws.rs.core.Response;
 @RequestScoped
 @Path("wm")
 public class RequestProcessor {
-    
+
+    TaskProcessor taskProcessor;
+    TwmTaskProcessor twmTaskProcessor;
+
     @PostConstruct
     void init() {
-        
-        twmTaskProcessor = new TwmTaskProcessor();
-        System.out.println("TwmTaskProcessor created");
-    }
-    
-    @Inject
-    TaskProcessor taskProcessor;
 
-    //  @Inject
-    TwmTaskProcessor twmTaskProcessor;
-    
+        twmTaskProcessor = new TwmTaskProcessor();
+        taskProcessor = new TaskProcessor();
+        System.out.println("TwmTaskProcessors created");
+    }
+
+    @GET
+    @Path("deadlock")
+    public Response goIntoDeadlock() {
+        TaskProcessor.deadLock = true;
+        return Response.ok("deadlockig now").build();
+    }
+
     @GET
     @Path("no_twm")
     public Response submitTask() {
+
         taskProcessor.doTask();
         return Response.ok().build();
     }
-    
+
     @GET
     @Path("twm")
     public Response submitTwmTask() {
-        
+
         try {
+
             twmTaskProcessor.doTask();
         } catch (InterruptedException ex) {
             return Response.serverError().entity(ex).build();
         }
         return Response.ok().build();
     }
-    
+
     @GET
     @Path("status")
     public Response status() {
-        
-        return Response.ok(twmTaskProcessor.statusToCountMap).build();
+
+        return Response.ok(TwmTaskProcessor.statusToCountMap).build();
     }
-    
+
     @GET
     @Path("statusHist")
     public Response statusHist(@QueryParam("id") Integer id) {
-        
-        return Response.ok(""+twmTaskProcessor.workItemToStatusHistoryMap.get(id)).build();
+
+        return Response.ok("" + TwmTaskProcessor.workItemToStatusHistoryMap.get(id)).build();
     }
 }
--- src/main/java/nl/ortecfinance/opal/weblogicworkmanager/TaskProcessor.java
+++ src/main/java/nl/ortecfinance/opal/weblogicworkmanager/TaskProcessor.java
@@ -2,17 +2,53 @@ package nl.ortecfinance.opal.weblogicworkmanager;
 
 import java.util.Date;
 import javax.ejb.Stateless;
+import org.apache.log4j.Logger;
 
 @Stateless
 public class TaskProcessor {
 
+    private static final Logger LOGGER = Logger.getLogger(TaskProcessor.class);
+    static boolean deadLock;
+
+    static final byte[] lock = new byte[0];
+    static final byte[] logLock = new byte[0];
+
+    static volatile int counter;
+    private static final int INITIAL_VALUE = -10;
+    static volatile int counterWhenDeadlocked = INITIAL_VALUE;
+
+    boolean deadlockInstance;
+
     public void doTask() {
+        counter++;
         try {
+            synchronized (lock) {
+                if (deadLock || counter <= counterWhenDeadlocked + 2) {
+                    deadLock = false;
+                    LOGGER.info("entering deadlock for counter=" + counter);
+                    if (counterWhenDeadlocked == INITIAL_VALUE) {
+                        LOGGER.info("Initiating  deadlock");
+                        counterWhenDeadlocked = counter;
+                    }
+
+                    deadlockInstance = true;
+                }
+            }
+
+            if (deadlockInstance) {
+                synchronized (logLock) {
+                    LOGGER.info("Locked on counter=" + counter);
+
+                    deadlockInstance = true;
+                    logLock.wait();
+                }
+            }
+
             Thread.sleep(3000);
         } catch (InterruptedException ex) {
-            System.out.println("Sleep failed");
+            LOGGER.info("Sleep failed");
         }
-        System.out.println("Doing task @" + new Date());
+        LOGGER.info("Doing task @" + new Date() + ", counter=" + counter);
     }
 
 }
